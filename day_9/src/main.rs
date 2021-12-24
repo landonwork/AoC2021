fade0ac6348693036cd73d6557ade41fd31f4b03
landonwork <landon.r.work@gmail.com>
2021-12-24 00:31:32 -0700

    Day 9 Part 2

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -1,8 +1,10 @@
 use std::fs;
 
+type Map = Vec<Vec<u32>>;
+
 fn main() {
     let raw = fs::read_to_string("input.txt").unwrap();
-    let map: Vec<Vec<u32>> = raw
+    let map: Map = raw
         .trim()
         .split('\n')
         .map(
@@ -19,9 +21,68 @@ fn main() {
         }
     }
     println!("Part 1: {}", risk);
+
+    println!("Part 2: {}", part2(&map));
 }
 
-fn is_low_point(x: usize, y: usize, map: &Vec<Vec<u32>>) -> bool {
+fn part2(map: &Map) -> u128 {
+    let mut basins: Map = vec![vec![0; map[0].len()]; map.len()];
+    let mut row = 0usize;
+    let mut col = 0usize;
+    let mut n_basins = 0u32;
+    let w = map[0].len();
+    let h = map.len();
+
+    while col < map.len() {
+        if basins[row][col] != 0 || map[row][col] == 9 {
+            row = (row + 1) % map.len();
+            if row == 0 { col += 1; }
+            continue
+        }
+        n_basins += 1;
+        explore_basin(row, col, map, &mut basins, n_basins);
+    }
+
+    let mut basin_sizes: Vec<u32> = vec![0; n_basins as usize];
+    for row in 0..h {
+        for col in 0..w {
+            if basins[row][col] != 0 {
+                basin_sizes[(basins[row][col] - 1) as usize] += 1
+            }
+        }
+    }
+    basin_sizes.sort();
+    basin_sizes.reverse();
+    basin_sizes[0..3].iter().fold(1, |a,b| a* (*b as u128))
+}
+
+fn explore_basin(row: usize, col: usize, map: &Map,
+                 basins: &mut Map, n: u32) -> () {
+
+    let w = map[0].len();
+    let h = map.len();
+
+    if row >= h || col >= w {
+        return ()
+    }
+
+    if map[row][col] == 9 || basins[row][col] != 0 {
+        return ()
+    }
+    
+    basins[row][col] = n;
+
+    if row != 0 {
+        explore_basin(row-1, col, map, basins, n);
+    }
+    explore_basin(row+1, col, map, basins, n);
+    if col != 0 {
+        explore_basin(row, col-1, map, basins, n);
+    }
+    explore_basin(row, col+1, map, basins, n);
+}
+
+fn is_low_point(x: usize, y: usize, map: &Map) -> bool {
 
     let w: usize = map[0].len();
     let h: usize = map.len();
@@ -31,7 +92,7 @@ fn is_low_point(x: usize, y: usize, map: &Vec<Vec<u32>>) -> bool {
             return false;
         }
     }
-    if x != w - 1 {
+    if x != h - 1 {
         if map[x][y] >= map[x + 1][y] {
             return false;
         }
@@ -42,7 +103,7 @@ fn is_low_point(x: usize, y: usize, map: &Vec<Vec<u32>>) -> bool {
             return false;
         }
     }
-    if y != h - 1 {
+    if y != w - 1 {
         if map[x][y] >= map[x][y + 1] {
             return false;
         }
