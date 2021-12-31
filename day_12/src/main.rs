e79ba4e3b9281f269deab07d0333fd7cea4b3414
landonwork <landon.r.work@gmail.com>
2021-12-30 23:33:11 -0700

    Day 12, Part 1

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,3 +1,61 @@
+use std::fs;
+use std::collections::HashMap;
+use regex::Regex;
+
+#[macro_use]
+extern crate lazy_static;
+
 fn main() {
-    println!("Hello, world!");
+    let raw: String = fs::read_to_string("input.txt").unwrap();
+    let lines: Vec<&str> = raw.trim().split('\n').collect();
+    let mut map: HashMap<&str, Vec<&str>> = HashMap::new();
+
+    for line in lines {
+        let t: Vec<&str> = line.split('-').collect();
+        add_to_map((t[0], t[1]), &mut map);
+    }
+
+    println!("Part 1: {}", part1(&map));
+    // println!("Part 2: {}", part2(&map));
+}
+
+fn part1(map: &HashMap<&str, Vec<&str>>) -> usize {
+    let mut n_paths = 0usize;
+    find_paths(vec!["start"], map, &mut n_paths);
+    n_paths
+}
+
+fn find_paths(path: Vec<&str>, map: &HashMap<&str, Vec<&str>>, count: &mut usize) {
+    for dest in map.get(&path[path.len()-1]).unwrap() {
+        if dest == &"end" {
+            *count += 1;
+            continue;
+        }
+        if is_small(dest) && path.contains(dest) {
+            continue;
+        }
+        let mut new_path = path.clone();
+        new_path.push(dest);
+        find_paths(new_path, map, count);
+    }
+}
+
+fn add_to_map<'a, 'b>((a, b): (&'a str, &'a str), map: &'b mut HashMap<&'a str, Vec<&'a str>>) -> () {
+    if map.contains_key(&a) {
+        map.get_mut(&a).unwrap().push(b);
+    } else {
+        map.insert(a, vec![b]);
+    }
+    if map.contains_key(&b) {
+        map.get_mut(&b).unwrap().push(a);
+    } else {
+        map.insert(b, vec![a]);
+    }
+}
+
+fn is_small(cave: &str) -> bool {
+    lazy_static! {
+        static ref SMALL: Regex = Regex::new(r"^[a-z]+$").unwrap();
+    }
+    SMALL.is_match(cave)
 }
