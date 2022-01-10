a7a4ca034e53a9a8ccebf5ea3529d557cb58e495
landonwork <landon.r.work@gmail.com>
2022-01-09 21:31:59 -0700

    I successfully created a Struct and implemeted the Index trait for it.

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -1,3 +1,42 @@
+use std::ops::Index;
+use std::marker::Copy;
+
+struct Map<T: Copy> {
+    m: Vec<Vec<T>>
+}
+
+impl Index<(usize, usize)> for Map<u32>
+{
+    type Output = u32;
+
+    fn index(&self, (row, col): (usize, usize)) -> &u32 {
+        &self.m[row][col]
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let raw: String = std::fs::read_to_string("input.txt").unwrap();
+    let rows: Vec<&str> = raw.trim().split('\n').collect();
+    let m: Vec<Vec<u32>> = rows.into_iter().map(
+        |row| row.chars().map(|c| c.to_digit(10).unwrap()).collect()
+        ).collect();
+    let map: Map<u32> = Map { m };
+
+    // println!("{:?}", get_shape(&map));
+    assert_eq!(7, map[(1,2)]);
+    // println!("Part 1: {}", part1(&map));
+    // println!("Part 2: {}", part2(&map));
+}
+
+fn get_shape<T: Copy>(map: Map<T>) -> (usize, usize) {
+    (map.m.len(), map.m[0].len()) // (rows, cols)
+}
+
+fn part1(m: &Vec<Vec<u32>>) -> u32 {
+    10
+}
+
+fn part2(m: &Vec<Vec<u32>>) -> u32 {
+    10
 }
